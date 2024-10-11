c3021e10f2da38fc51e219c2e3aaddbe71d40dba
Etienne Grzesiek <etienne42g@gmail.com>
2024-10-11 21:19:49 +0200

    fix https callback google

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: 'https://santa.etiennegrz.fr/auth/google/callback'
   }, async (accessToken, refreshToken, profile, done) => {
     try {
       const email = profile.emails[0].value;
